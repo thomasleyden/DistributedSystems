f77782c97e1cb6154c357e0396c05639d6479a09
Thomas Leyden <thomasleyden1@gmail.com>
2022-09-23 00:07:32 -0500

    Fixup formatting, also input sanitization

--- hw1/Client.java
+++ hw1/Client.java
@@ -3,7 +3,7 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     String hostAddress;
     int tcpPort;
     int udpPort;
@@ -24,43 +24,65 @@ public class Client {
 
     // Setup the Socket to the server
     try (Socket socket = new Socket(hostAddress, tcpPort)) {
-          Scanner sc = new Scanner(System.in);
-          out = new PrintWriter(socket.getOutputStream(), true);
-          in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-          String serverResponse;
+      Scanner sc = new Scanner(System.in);
+      out = new PrintWriter(socket.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      String serverResponse;
 
-          while(sc.hasNextLine()) {
-            String cmd = sc.nextLine();
-            String[] tokens = cmd.split(" ");
+      while (sc.hasNextLine()) {
+        String cmd = sc.nextLine();
+        String[] tokens = cmd.split(" ");
 
-            if (tokens[0].equals("setmode")) {
-              // TODO: set the mode of communication for sending commands to the server 
-              // and display the name of the protocol that will be used in future
-            }
-            else if (tokens[0].equals("purchase")) {
-              out.println(cmd);
-            } else if (tokens[0].equals("cancel")) {
-              out.println(cmd);
-            } else if (tokens[0].equals("search")) {
-              out.println(cmd);
-            } else if (tokens[0].equals("list")) {
-              out.println(cmd);
-            } else {
-              System.out.println("ERROR: No such command");
-              continue;
-            }
-
-            // Print out the server response
-            while (((serverResponse = in.readLine()) != null) && (!serverResponse.equals("EOR"))) {
-              System.out.println(serverResponse);
-            }
+        if (tokens[0].equals("setmode")) {
+          // TODO: set the mode of communication for sending commands to the server
+          // and display the name of the protocol that will be used in future
+        } else if (tokens[0].equals("purchase")) {
+          if (tokens.length != 4) {
+            continue;
           }
-        } catch (UnknownHostException ex) {
-            System.out.println("Server not found: " + ex.getMessage());
-        } catch (IOException ex) {
-            System.out.println("I/O error: " + ex.getMessage());
+          try {
+            Integer.parseInt(tokens[3]);
+          } catch (NumberFormatException nfe) {
+            System.out.println("Purchase Quantity not an Int");
+            continue;
+          }
+          out.println(cmd);
+        } else if (tokens[0].equals("cancel")) {
+          if (tokens.length != 2) {
+            continue;
+          }
+          try {
+            Integer.parseInt(tokens[1]);
+          } catch (NumberFormatException nfe) {
+            System.out.println("Order Number not an Int");
+            continue;
+          }
+          out.println(cmd);
+        } else if (tokens[0].equals("search")) {
+          if (tokens.length != 2) {
+            continue;
+          }
+          out.println(cmd);
+        } else if (tokens[0].equals("list")) {
+          if (tokens.length != 1) {
+            continue;
+          }
+          out.println(cmd);
+        } else {
+          System.out.println("ERROR: No such command");
+          continue;
         }
 
+        // Print out the server response
+        while (((serverResponse = in.readLine()) != null) && (!serverResponse.equals("EOR"))) {
+          System.out.println(serverResponse);
+        }
+      }
+    } catch (UnknownHostException ex) {
+      System.out.println("Server not found: " + ex.getMessage());
+    } catch (IOException ex) {
+      System.out.println("I/O error: " + ex.getMessage());
+    }
 
   }
 }
